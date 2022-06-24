2838988e1f8d546363e591e974adc063ad5d7e2f
Ajajahmad <88666422+Ajajahmad@users.noreply.github.com>
2022-06-24 17:39:17 +0530

    Added solution - LeetHub

---
+++ Shortest path from 1 to n - GFG/shortest-path-from-1-to-n.cpp
@@ -0,0 +1,35 @@
+// { Driver Code Starts
+//Initial template for c++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int minimumStep(int n){
+       if(n==1) return 0;
+       return (n%3==0)? 1+minimumStep(n/3): 1+minimumStep(n-1);
+    }
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.minimumStep(n) << endl;
+    
+    }
+    return 0; 
+} 
+
+  // } Driver Code Ends
