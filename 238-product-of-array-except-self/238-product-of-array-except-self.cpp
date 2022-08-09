905f0168a60a074d6c4c5c3e43c249eea3f246cf
Ajajahmad <88666422+Ajajahmad@users.noreply.github.com>
2022-08-09 20:10:23 +0530

    Time: 55 ms (15.14%), Space: 24.1 MB (52.33%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int mul =1;
+        int zero=0;
         int n = nums.size();
-        vector<int> res(n,1);
-        for(int i=n-2;i>=0;i--)
+        int pro = 1;
+        for(int i=0;i<n;i++)
         {
-            res[i] = res[i+1]*nums[i+1];
-            cout<<res[i]<<" ";
+            if(nums[i]==0) zero++;
+            if(nums[i]!=0) pro = pro*nums[i];
         }
-        for(int i=1;i<n;i++)
+        for(int i =0;i<n;i++)
         {
-            res[i] = res[i]*(mul*nums[i-1]);
-            mul =mul*nums[i-1];
+            if(zero>1 ) nums[i]=0;
+            else if(zero==0 ) nums[i] = pro/nums[i];
+            else if(zero==1 && nums[i]==0) nums[i] = pro;
+            else if(zero==1 && nums[i]!=0) nums[i] = 0; 
         }
-        return res;
+        return nums;
     }
 };
