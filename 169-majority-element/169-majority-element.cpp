279715211e5a64d648aa9efa61559b6918f7ace9
Ajajahmad <88666422+Ajajahmad@users.noreply.github.com>
2022-08-11 14:12:08 +0530

    Time: 21 ms (86.53%), Space: 19.6 MB (13.21%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -8,23 +8,24 @@ public:
             if(ele == a[i])
             {
                 count++;
-                ele = a[i];
             }
-            else
+            else if(ele!=a[i])
             {
                 count--;
-            }
-            if(count ==0){
-                count=1;
-                ele = a[i];
+                if(count == 0)
+                {
+                    count=1;
+                    ele = a[i];
+                }
             }
         }
-        count=0;
+        count =0;
         for(int i=0;i<size;i++)
         {
-            if(a[i]==ele)
-            count++;
+           if(a[i]==ele) count++;
         }
-        return (count>size/2)? ele : -1;
+        if(count> size/2)
+            return ele;
+        return -1;
     }
 };
