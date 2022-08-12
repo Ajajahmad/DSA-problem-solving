89a730613e7cd6822786cc1715a5c4e2df8f14c3
Ajajahmad <88666422+Ajajahmad@users.noreply.github.com>
2022-08-12 17:59:42 +0530

    Time: 55 ms (52.66%), Space: 13.3 MB (29.05%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size(), m=n ? grid[0].size() :0 ;
+        int n = grid.size(), m= grid[0].size() ;
         int ans = 0;
         vector<vector<int>> vis(n, vector<int>(m,0));
         for(int i =0;i<n;i++)
