08eca8eb8c23e25c85f308bde676d331c33de246
Ajajahmad <88666422+Ajajahmad@users.noreply.github.com>
2022-08-19 15:30:50 +0530

    Time: 367 ms (77.94%), Space: 8 MB (41.02%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -16,9 +16,9 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         int n =board.size(), m = board[0].size();
         
-        for(unsigned  int i=0;i<n;i++)
+        for( int i=0;i<n;i++)
         {
-            for(unsigned  int j=0;j<m;j++)
+            for(int j=0;j<m;j++)
             {
                 if(solve(board , word, i,j, 0))
                     return true;
