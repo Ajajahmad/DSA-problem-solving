ae0a800e38385ca52bce508f99576823202a73de
Ajajahmad <88666422+Ajajahmad@users.noreply.github.com>
2022-09-02 14:41:30 +0530

    Time: 0 ms (100.00%), Space: 12 MB (93.03%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,49 +1,25 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-       int n = nums.size();
-        int i = n-2;
-        while(i>=0)
-        {
-            if(nums[i]>=nums[i+1]) i--;
-            else 
+        int n= nums.size();
+        if(n==1) return;
+        int i,k;
+        for( i=n-2;i>=0;i--){
+            if(nums[i] < nums[i+1])
             {
-                int j = -1;
-                int v = INT_MAX;
-                int k = i+1;
-                while(k<n)
-                {
-                    if(nums[k]>nums[i] && nums[k] <=v)
-                        v= nums[k],j=k;
-                    k++;
-                }
-                int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                 j =n-1;
-                i++;
-                while(i<j)
-                {
-                    temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    i++;
-                    j--;
-                }
-                return;
+                break;
             }
         }
-        if(i<0)
-        {
-            int x=0,y=n-1;
-            while(x<y)
-            {
-                int temp = nums[x];
-                nums[x] = nums[y];
-                nums[y] = temp;
-                x++;
-                y--;
+        if(i<0) reverse(nums.begin(),nums.end());
+        else{
+            for(int j=n-1;j>i;j--){
+            if(nums[j]>nums[i]){
+                k=j;
+                break;
             }
+        }
+        swap(nums[i],nums[k]);
+        sort(nums.begin()+i+1,nums.end());
         }
     }
 };
