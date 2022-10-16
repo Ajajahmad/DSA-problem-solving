644987ed3ce6ae6daeb4426c10a9a1262fd4abab
Ajaj Ahmad <88666422+Ajajahmad@users.noreply.github.com>
2022-10-16 11:41:42 +0530

    Time: 1792 ms (51.15%), Space: 122.5 MB (84.55%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int countDistinct(vector<int>& nums, int k, int p) {
         int n = nums.size();
-        map<vector<int> , int> mp;
+        map<string , int> mp;
         int res=0;
         
         for(int i=0 ;i<n;i++)
         {
             int x = i, y= i;
             int dv = 0;
-            vector<int> temp;
+            string temp="";
             while(y<n)
             {
-                temp.push_back(nums[y]);
+                temp += nums[y]-'0';
                 if(nums[y]%p==0) dv++;
                 if(dv<=k && mp.find(temp)==mp.end())
                 {
