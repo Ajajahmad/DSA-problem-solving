c405c0da8b376397bd2501e8f147f3ba36f12801
Ajaj Ahmad <88666422+Ajajahmad@users.noreply.github.com>
2022-10-21 23:47:07 +0530

    Time: 12 ms (94.99%), Space: 10.7 MB (78.96%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,27 +1,54 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int longestCommonSubsequence(string s1, string s2) {
-        memset(dp,-1,sizeof(dp));
-        int n = s1.length();
-        int m = s2.length();
-        return solve(n,m,s1,s2);
-    }
-    int solve(int n, int m, string s1, string s2)
-    {
-        for(int i=0;i<n+1;i++) dp[i][0]=0;
-        for(int i=0;i<m+1;i++) dp[0][i]=0;
-        if(dp[n][m]!=-1) return dp[n][m];
+    int longestCommonSubsequence(string text1, string text2) {
+         int n=text1.length();
+        int m=text2.length();
+        int dp[n+1][m+1];
+        for(int i=0;i<n+1;i++) dp[i][0] = 0;
+        for(int i=0;i<m+1;i++) dp[0][i] = 0;
         for(int i=1;i<n+1;i++)
         {
             for(int j=1;j<m+1;j++)
             {
-                if(s1[i-1]==s2[j-1])
-                    dp[i][j] = 1+dp[i-1][j-1];
+                 if(text1[i-1]==text2[j-1])
+                     dp[i][j] = 1+dp[i-1][j-1];
                 else
-                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);        
+                {
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }
             }
         }
+        
+        
+        string res = "";
+        int i=n, j=m;
+        while(i>0 && j>0)
+        {
+            if(text1[i-1] == text2[j-1])
+            {
+                // cout<<text1[i-1];
+                res.push_back(text1[i-1]);
+                i--;
+                j--;
+            }
+            else
+            {
+                if(dp[i-1][j] > dp[i][j-1])
+                {
+                    
+                    i--;
+
+                }
+                else
+                {
+                   
+                    j--;
+                }
+            }
+        }
+        reverse(res.begin() , res.end());
+        if(res=="") cout<<"FUCK U ";
+        cout<<res<< " ";
         return dp[n][m];
     }
 };
