6e0dfef751cffe7a90030a3949e3b458db0e1d73
Ajaj Ahmad <88666422+Ajajahmad@users.noreply.github.com>
2022-11-07 22:43:16 +0530

    Time: 475 ms (25.00%), Space: 75.6 MB (25.00%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -5,41 +5,41 @@ public:
         int n = costs.size();
         int j  =  n-1;
         
-        priority_queue<pair<int,pair<int, int> > , vector<pair<int,pair<int, int> >> , greater<pair<int,pair<int, int> >> > pq;
+        priority_queue<pair<int,int > , vector<pair<int,int >> , greater<pair<int,int >> > pq;
         int i = 0;
         int count = c;
         while(i< n && count--)
         {
-            pq.push({costs[i] , {i, 0}});
+            pq.push({costs[i] , 0});
             i++;
         }
         count = c;
         
         while(j>i && count--)
         {
-            pq.push({costs[j] ,{ j , 1}});
+            pq.push({costs[j] ,1});
             j--;
         }
         while(i<=j && k)
         {
-            pair<int , pair<int,int>> val = pq.top();
+            pair<int , int > val = pq.top();
             pq.pop();
             sum+=val.first;
-            if(val.second.second == 0)
+            if(val.second == 0)
             {
-                pq.push({costs[i] , {i, 0}});
+                pq.push({costs[i] ,0});
                 i++;
             }
             else 
             {
-                pq.push({costs[j] , { j , 1}});
+                pq.push({costs[j] , 1});
                 j--;
             }
             k--;
         }
         while(k--)
         {
-            pair<int , pair<int,int>> val = pq.top();
+            pair<int ,int> val = pq.top();
             pq.pop();
             sum += val.first;
         }
